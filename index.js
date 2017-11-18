9ad4a002764ffd0b680edf2a0dc07126a1eb4a3b
Chen Wen <joc98@163.com>
2017-11-18 22:32:55 +0800

    change the 2nd argument as

--- index.js
+++ index.js
@@ -4,15 +4,29 @@
 
 const traverser = require('dir-traverse');
 
-const load = (dir, filter) => {
+const load = (dir, options) => {
     const modules = [];
-    const handler = ({fullPath}) => modules.push(require(fullPath));
-    traverser(dir, {filter, handler});
+    const handler = (attrs) => {
+        const {fullPath} = attrs;
+        let mo = require(fullPath);
+        if (options.handler) {
+            mo = options.handler(attrs, mo) || mo;
+        }
+        modules.push(mo);
+    };
+    traverser(dir, {filter: options.filter, handler});
     return modules;
 };
 
-const loader = (dirs, filter) => {
-    const fn = dir => load(dir, filter);
+/**
+ * @param dirs
+ * @param options {?object=}
+ * @param options.filter {?string|regexp|Array.<string|regexp>|object=}
+ * @param options.handler
+ * @returns {*}
+ */
+const loader = (dirs, {filter, handler} = {}) => {
+    const fn = dir => load(dir, {filter, handler});
     if (Array.isArray(dirs)) {
         return dirs.map(fn);
     }
--- test/loader.js
+++ test/loader.js
@@ -24,34 +24,34 @@ describe('loader()', () => {
 
     it('filter files or directories by a string pattern', () => {
         const path = `${dir}/m2`;
-        const file = 'm22.js';
-        assert.sameDeepOrderedMembers(loader(path, file), getExpected(path, [file]));
+        const filter = 'm22.js';
+        assert.sameDeepOrderedMembers(loader(path, {filter}), getExpected(path, [filter]));
     });
 
     it('filter files or directories by a regex', () => {
         const path = `${dir}/m2`;
         const filter = /m22\.js/;
         const file = 'm22.js';
-        assert.sameDeepOrderedMembers(loader(path, filter), getExpected(path, [file]));
+        assert.sameDeepOrderedMembers(loader(path, {filter}), getExpected(path, [file]));
     });
 
     it('filter by multi patterns', () => {
         const path = `${dir}/m1`;
         const filter = [/m12/, 'm11'];
-        assert.sameDeepOrderedMembers(loader(path, filter), getExpected(path, ['m11', 'm12.js']));
+        assert.sameDeepOrderedMembers(loader(path, {filter}), getExpected(path, ['m11', 'm12.js']));
     });
 
     it('filter by a function', () => {
         const path = `${dir}/m1`;
         const filter = file => 'm11' === file;
-        assert.sameDeepOrderedMembers(loader(path, filter), getExpected(path, ['m11']));
+        assert.sameDeepOrderedMembers(loader(path, {filter}), getExpected(path, ['m11']));
     });
 
     it('load multi dirs', () => {
         const dirs = ['m1', 'm2'].map(p => `${dir}/${p}`);
         const filter = file => file !== 'm21';
         const expected = [getExpected(dirs[0], ['m11', 'm12']), getExpected(dirs[1], ['m22'])];
-        assert.sameDeepOrderedMembers(loader(dirs, filter), expected);
+        assert.sameDeepOrderedMembers(loader(dirs, {filter}), expected);
     });
 
     it('load multi dirs from an object', () => {
@@ -61,6 +61,13 @@ describe('loader()', () => {
         });
         const filter = file => file !== 'm21';
         const expected = {m1: getExpected(dirs.m1, ['m11', 'm12']), m2: getExpected(dirs.m2, ['m22'])};
-        assert.deepEqual(loader(dirs, filter), expected);
+        assert.deepEqual(loader(dirs, {filter}), expected);
+    });
+
+    it('rewrite the module by `options.handler`', () => {
+        const path = `${dir}/m1`;
+        const filter = file => 'm11' === file;
+        // assert.sameDeepOrderedMembers(loader(path, {filter}), getExpected(path, ['m11']));
+        assert.deepEqual(loader(path, {filter, handler: () => 1}), [1])
     });
 });
